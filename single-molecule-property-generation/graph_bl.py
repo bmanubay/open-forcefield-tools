789844a636e259d2574e8252b058dd0e699c9d9f
Bryce Manubay <brma3379@login04.rc.colorado.edu>
2017-04-18 11:14:47 -0600

    added contour plots of observables

--- single-molecule-property-generation/graph_bl.py
+++ single-molecule-property-generation/graph_bl.py
@@ -9,6 +9,7 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 import scipy as sp
+from scipy.interpolate import griddata
 import seaborn as sns
 
 from scipy.stats import norm
@@ -23,10 +24,38 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 ax.scatter(df.k_values,df.length_values,df.bond_length_average)
+ax.set_xlabel('Bonded force constant - (kcal/mol/A^2)')
+ax.set_ylabel('Equilibrium bond length - (A)')
+ax.set_zlabel('Average of bond length distribution - (A)')
 plt.savefig('bond_length_average_vs_k_length.png')
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 ax.scatter(df.k_values,df.length_values,df.bond_length_variance)
+ax.set_xlabel('Bonded force constant - (kcal/mol/A^2)')
+ax.set_ylabel('Equilibrium bond length - (A)')
+ax.set_zlabel('Variance of bond length distribution - (A^2)')
 plt.savefig('bond_length_variance_vs_k_length.png')
+
+
+grid_x, grid_y = np.mgrid[0:1200:100j, 0:1.75:200j]
+
+z1 = griddata([[i,j] for i,j in zip(df.k_values,df.length_values)], df.bond_length_average, (grid_x, grid_y), method='cubic')
+z2 = griddata([[i,j] for i,j in zip(df.k_values,df.length_values)], df.bond_length_variance, (grid_x, grid_y), method='cubic')
+
+plt.figure()
+CS = plt.contour(grid_x,grid_y,z1)
+plt.clabel(CS, inline=1, fontsize=10)
+plt.xlabel('Bonded force constant - (kcal/mol/A^2)')
+plt.ylabel('Equilibrium bond length - (A)')
+plt.title('Average of bond length distribution vs k_Bond and x_0 - (A)')
+plt.savefig('bond_length_average_vs_k_length_contour.png')
+
+plt.figure()
+CS = plt.contour(grid_x,grid_y,z2)
+plt.clabel(CS, inline=1, fontsize=10)
+plt.xlabel('Bonded force constant - (kcal/mol/A^2)')
+plt.ylabel('Equilibrium bond length - (A)')
+plt.title('Variance of bond length distribution vs k_Bond and x_0 - (A^2)')
+plt.savefig('bond_length_variance_vs_k_length_contour.png')
